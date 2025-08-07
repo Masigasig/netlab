7a900d302929ee051d2ca2f3104558f497df8c7e
luckstraw <aviles.jamesluck.ecc.bscs@gmail.com>
2025-08-08 07:59:08 +0800

    chore: add SimObjNotifier Access to ref

--- lib/simulation/sim_screen_state/switch_notifier.dart
+++ lib/simulation/sim_screen_state/switch_notifier.dart
@@ -2,13 +2,15 @@ part of 'sim_screen_state.dart';
 
 final switchProvider =
     StateNotifierProvider<SwitchNotifier, Map<String, Switch>>(
-      (ref) => SwitchNotifier(),
+      (ref) => SwitchNotifier(ref),
     );
 final switchWidgetProvider =
     StateNotifierProvider<SwitchWidgetNotifier, Map<String, SwitchWidget>>(
       (ref) => SwitchWidgetNotifier(),
     );
 
-class SwitchNotifier extends DeviceNotifier<Switch> {}
+class SwitchNotifier extends DeviceNotifier<Switch> {
+  SwitchNotifier(super.ref);
+}
 
 class SwitchWidgetNotifier extends DeviceWidgetNotifier<SwitchWidget> {}
