b497c75f9ab6a3e9d437b573632de936ccb02f65
luckstraw <aviles.jamesluck.ecc.bscs@gmail.com>
2025-08-08 08:05:07 +0800

    chore: add MessgaeNotifier

---
+++ lib/simulation/sim_screen_state/message_notifier.dart
@@ -0,0 +1,38 @@
+part of 'sim_screen_state.dart';
+
+final messageProvider =
+    StateNotifierProvider<MessageNotifier, Map<String, Message>>(
+      (ref) => MessageNotifier(ref),
+    );
+
+class MessageNotifier extends SimObjectNotifier<Message> {
+  MessageNotifier(super.ref);
+
+  void pushLayer(String messageId, Map<String, String> newLayer) {
+    final message = state[messageId]!;
+
+    final newStack = List<Map<String, String>>.from(message.layerStack)
+      ..add(newLayer);
+
+    final updatedMessage = message.copyWith(layerStack: newStack);
+
+    state = {...state, messageId: updatedMessage};
+  }
+
+  void popLayer(String messageId) {
+    final message = state[messageId]!;
+
+    final newStack = List<Map<String, String>>.from(message.layerStack)
+      ..removeLast();
+
+    final updatedMessage = message.copyWith(layerStack: newStack);
+
+    state = {...state, messageId: updatedMessage};
+  }
+
+  void updateCurrentPlaceId(String messageId, String newPlace) {
+    final message = state[messageId]!;
+
+    state = {...state, messageId: message.copyWith(currentPlaceId: newPlace)};
+  }
+}
