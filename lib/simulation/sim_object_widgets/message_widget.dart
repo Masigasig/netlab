441d2c956ad3226d12a0522614331197f4137d7f
luckstraw <aviles.jamesluck.ecc.bscs@gmail.com>
2025-08-15 08:33:57 +0800

    chore: messageWidget

---
+++ lib/simulation/sim_object_widgets/message_widget.dart
@@ -0,0 +1,63 @@
+part of 'sim_object_widget.dart';
+
+class MessageWidget extends SimObjectWidget {
+  final String imagePath = AppImage.message;
+  final double size = 50;
+
+  const MessageWidget({super.key, required super.simObjectId});
+
+  factory MessageWidget.fromId(String simObjectId) {
+    return MessageWidget(simObjectId: simObjectId);
+  }
+
+  @override
+  ConsumerState<MessageWidget> createState() => _MessageWidgetState();
+}
+
+class _MessageWidgetState extends _SimObjectWidgetState<MessageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final message = ref.watch(messageProvider(widget.simObjectId));
+
+    Column messageWithLabel() {
+      final label = message.dstId.startsWith(DataLinkLayerType.arp.name)
+          ? 'ARP'
+          : message.name;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Material(
+            color: Colors.transparent,
+            child: Text(
+              label,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+            ),
+          ),
+          SizedBox(
+            width: widget.size,
+            height: widget.size,
+            child: Image.asset(widget.imagePath, fit: BoxFit.contain),
+          ),
+        ],
+      );
+    }
+
+    return AnimatedPositioned(
+      left: message.posX,
+      top: message.posY,
+      duration: message.duration,
+      curve: Curves.easeIn,
+      child: GestureDetector(onTap: _handleTap, child: messageWithLabel()),
+      onEnd: () {
+        if (message.currentPlaceId.startsWith(SimObjectType.connection.label)) {
+          ref
+              .read(connectionProvider(message.currentPlaceId).notifier)
+              .receiveMessage(widget.simObjectId);
+        }
+      },
+    );
+  }
+
+  void _handleTap() {}
+}
