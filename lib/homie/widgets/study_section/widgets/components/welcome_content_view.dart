fd3fca666f05f45059dc77a60e91aa8baab6abbe
Fuminnn <michealpo567@gmail.com>
2025-08-26 19:21:10 +0800

    update welcome_cpntent_view

--- lib/homie/widgets/study_section/widgets/components/welcome_content_view.dart
+++ lib/homie/widgets/study_section/widgets/components/welcome_content_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../models/study_topic.dart';
+import 'package:netlab/core/constants/app_text.dart';
 
 class WelcomeContentView extends StatelessWidget {
   final StudyTopic topic;
@@ -12,76 +13,66 @@ class WelcomeContentView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 120,
-            height: 120,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  topic.cardColor,
-                  topic.cardColor.withOpacity(0.6),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Title
+            Text(
+              'Welcome to ${topic.title}',
+              style: AppTextStyles.headerMedium.copyWith(
+                fontSize: 26,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 16),
+
+            // Description
+            Text(
+              topic.description,
+              style: AppTextStyles.bodyMedium.copyWith(
+                fontSize: 16,
+                color: Colors.white.withOpacity(0.85),
+                height: 1.6,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 32),
+
+            // Instruction box
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 14),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.08),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.arrow_back,
+                    color: Colors.white.withOpacity(0.7),
+                    size: 18,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    'Select a module from the sidebar to begin',
+                    style: AppTextStyles.bodySmall.copyWith(
+                      fontSize: 14,
+                      color: Colors.white.withOpacity(0.85),
+                    ),
+                  ),
                 ],
               ),
             ),
-            child: Icon(
-              topic.icon,
-              color: Colors.white,
-              size: 60,
-            ),
-          ),
-          const SizedBox(height: 32),
-          Text(
-            'Welcome to ${topic.title}',
-            style: const TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 16),
-          Text(
-            topic.description,
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.white.withOpacity(0.8),
-              height: 1.5,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 32),
-          Container(
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  Icons.arrow_back,
-                  color: Colors.white.withOpacity(0.7),
-                  size: 20,
-                ),
-                const SizedBox(width: 12),
-                Text(
-                  'Select a module from the sidebar to begin',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.white.withOpacity(0.8),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
