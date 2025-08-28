dc12cf60bb3f68f39ab2b56fc16dc7086ac9f242
luckstraw <aviles.jamesluck.ecc.bscs@gmail.com>
2025-08-28 16:31:13 +0800

    feat: can connect to router

--- lib/simulation/sim_object_notifiers/router_notifier.dart
+++ lib/simulation/sim_object_notifiers/router_notifier.dart
@@ -16,13 +16,75 @@ class RouterNotifier extends DeviceNotifier<Router> {
 
   @override
   List<Map<String, String>> getAllConnectionInfo() {
-    // TODO: implement getAllConnectionInfo
-    throw UnimplementedError();
+    return [
+      {
+        ConnectionInfoKey.name.name: Eth.eth0.name,
+        ConnectionInfoKey.conId.name: state.eth0conId,
+      },
+      {
+        ConnectionInfoKey.name.name: Eth.eth1.name,
+        ConnectionInfoKey.conId.name: state.eth1conId,
+      },
+      {
+        ConnectionInfoKey.name.name: Eth.eth2.name,
+        ConnectionInfoKey.conId.name: state.eth2conId,
+      },
+      {
+        ConnectionInfoKey.name.name: Eth.eth3.name,
+        ConnectionInfoKey.conId.name: state.eth3conId,
+      },
+    ];
   }
 
   @override
   void removeSelf() {
-    // TODO: implement removeSelf
+    if (state.eth0conId.isNotEmpty) {
+      connectionNotifier(state.eth0conId).removeSelf();
+    }
+
+    if (state.eth1conId.isNotEmpty) {
+      connectionNotifier(state.eth1conId).removeSelf();
+    }
+
+    if (state.eth2conId.isNotEmpty) {
+      connectionNotifier(state.eth2conId).removeSelf();
+    }
+
+    if (state.eth3conId.isNotEmpty) {
+      connectionNotifier(state.eth3conId).removeSelf();
+    }
+
+    routerMapNotifier.removeAllState(state.id);
+  }
+
+  void updateConIdByEthName(String ethName, String newConId) {
+    final eth = Eth.values.firstWhere((e) => e.name == ethName);
+    switch (eth) {
+      case Eth.eth0:
+        state = state.copyWith(eth0conId: newConId);
+        break;
+      case Eth.eth1:
+        state = state.copyWith(eth1conId: newConId);
+        break;
+      case Eth.eth2:
+        state = state.copyWith(eth2conId: newConId);
+        break;
+      case Eth.eth3:
+        state = state.copyWith(eth3conId: newConId);
+        break;
+    }
+  }
+
+  void removeConIdByConId(String conId) {
+    if (state.eth0conId == conId) {
+      state = state.copyWith(eth0conId: '');
+    } else if (state.eth1conId == conId) {
+      state = state.copyWith(eth1conId: '');
+    } else if (state.eth2conId == conId) {
+      state = state.copyWith(eth2conId: '');
+    } else if (state.eth3conId == conId) {
+      state = state.copyWith(eth3conId: '');
+    }
   }
 }
 
